2d49c7f39788bb7541e4ae60b9ace73ac39e95da
Clinton James <5165018+cptwobbles@users.noreply.github.com>
2020-06-19 11:44:08 +1200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -136,7 +136,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "824deb44-1aa4-4c03-905d-ee0ea5279ad5"
 
 // @section extruder
 
