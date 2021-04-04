667471afa8eae5a325ab9e6898b99fe3ebc95288
Clinton James <5165018+cptwobbles@users.noreply.github.com>
2021-04-04 12:02:11 +1200

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1994,10 +1994,10 @@
     #define MIN_AUTORETRACT 0.1           // (mm) Don't convert E moves under this length
     #define MAX_AUTORETRACT 10.0          // (mm) Don't convert E moves over this length
   #endif
-  #define RETRACT_LENGTH 2.5                // (mm) Default retract length (positive value)
+  #define RETRACT_LENGTH 2                // (mm) Default retract length (positive value)
   #define RETRACT_LENGTH_SWAP 10          // (mm) Default swap retract length (positive value)
   #define RETRACT_FEEDRATE 40             // (mm/s) Default feedrate for retracting
-  #define RETRACT_ZRAISE 0.48                // (mm) Default retract Z-raise
+  #define RETRACT_ZRAISE 0                // (mm) Default retract Z-raise
   #define RETRACT_RECOVER_LENGTH 0        // (mm) Default additional recover length (added to retract length on recover)
   #define RETRACT_RECOVER_LENGTH_SWAP 0   // (mm) Default additional swap recover length (added to retract length on recover from toolchange)
   #define RETRACT_RECOVER_FEEDRATE 8      // (mm/s) Default feedrate for recovering from retraction
