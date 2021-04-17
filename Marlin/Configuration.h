ac2b05ee57800331cd624d1f71db0e4d1d7691b5
Clinton James <5165018+cptwobbles@users.noreply.github.com>
2021-04-17 14:31:40 +1200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -168,7 +168,7 @@
  */
 //#define MK2_MULTIPLEXER
 #if ENABLED(MK2_MULTIPLEXER)
-  // Override the default DIO selector dns here, if needed.
+  // Override the default DIO selector pins here, if needed.
   // Some pins files may provide defaults for these pins.
   //#define E_MUX0_PIN 40  // Always Required
   //#define E_MUX1_PIN 42  // Needed for 3 to 8 inputs
