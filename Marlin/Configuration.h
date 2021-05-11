3c0206ccc2115f2ed2ed9486686de4221adf13b3
Clinton James <5165018+cptwobbles@users.noreply.github.com>
2021-05-11 15:26:13 +1200

    Updated probe
    Enabled meatpack

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1048,7 +1048,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 40, -47, 0 } // CJ
+#define NOZZLE_TO_PROBE_OFFSET { -23.5, -40, 0 } // CJ
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
